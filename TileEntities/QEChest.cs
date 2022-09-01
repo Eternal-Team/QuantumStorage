7b86e6d7d40d99430938e5ff2c992fa73c45e625
Itorius <itorius.yt@gmail.com>
2022-09-01 17:58:56 +0200

    Drop gems when breaking chest

--- TileEntities/QEChest.cs
+++ TileEntities/QEChest.cs
@@ -5,6 +5,8 @@ using BaseLibrary.UI;
 using BaseLibrary.Utility;
 using ContainerLibrary;
 using QuantumStorage.Global;
+using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
@@ -21,6 +23,12 @@ public class QEChest : BaseTileEntity, IItemStorage, IHasUI
 	{
 		ID = Guid.NewGuid();
 		Frequency = new Frequency();
+	}
+
+	public override void OnKill()
+	{
+		for (int index = 0; index < 3; index++)
+			Item.NewItem(new EntitySource_Misc("QuantumStorage.Reset"), Position.X * 16, Position.Y * 16, 32, 32, Utility.ColorToItem(Frequency[index]));
 	}
 
 	public override void SaveData(TagCompound tag)
